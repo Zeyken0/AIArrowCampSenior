c541a256ac5173c488c5e274884a22e1d953fad5
Zeyken_0 <azher2007@mail.ru>
2024-07-30 11:07:49 +0700

    created basic architecture for LLM interfaces

--- llm.py
+++ llm.py
@@ -1,16 +1,33 @@
 from langchain_community.chat_models import GigaChat
+from langchain.schema import HumanMessage, SystemMessage
 from config import GIGACHAT_CREDENTIALS
 
-chat = GigaChat(
-    model='GigaChat',
-    credentials=GIGACHAT_CREDENTIALS,
-)
-#, verify_ssl_certs=False, stream = True
 
-from langchain.schema import HumanMessage, SystemMessage
+class LLMObject:
+    def project_struct(self, content: str) -> str:
+        return 'Basic return'
 
-messages = [
-    HumanMessage(content='Привет!')
-]
+    def idea_struct(self, content: str) -> str:
+        return 'Basic return'
 
-print(chat.get_models())
+    def task_struct(self, content: str) -> str:
+        return 'Basic return'
+
+
+class GigaChatInterface(LLMObject):
+    def __init__(self):
+        self._model = GigaChat(
+            model='GigaChat',
+            credentials=GIGACHAT_CREDENTIALS,
+        )
+
+    def project_struct(self, content: str) -> str:
+        return ''
+
+    def idea_struct(self, content: str) -> str:
+        return ''
+
+    def task_struct(self, content: str) -> str:
+        return ''
+
+
